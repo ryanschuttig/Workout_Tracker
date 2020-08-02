ba8dea461dbd51791471806303c1b1349356f6ae
ryanschuttig <ryanschuttig@gmail.com>
2020-08-02 11:50:16 -0400

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = (app => {
+    app.get("/index", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+});
