6a2502ae7979716db6c63eae6b8334dc963c135c
ryanschuttig <ryanschuttig@gmail.com>
2020-08-03 19:37:54 -0400

    heroku

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
 });
 
+mongoose.connect(MONGODB_URI);
+
 // routes
 app.use(require("./routes/api-routes"));
 app.use(require("./routes/html-routes"));
